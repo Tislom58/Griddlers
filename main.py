10384167e60c76327d54f8371c37f50855faed1d
Daniel <danmuller.astro@gmail.com>
2023-12-16 13:39:49 +0100

    Added grid

--- main.py
+++ main.py
@@ -6,58 +6,80 @@ pygame.init()
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 CENTER = (WIDTH // 2, HEIGHT // 2)
+
 pygame.display.set_caption("Griddles")
 
 # Color presets
-WHITE = (255, 255, 255)
+WHITE = (229, 222, 207)
 BLACK = (0, 0, 0)
 BLUE = (100, 149, 237)
 
 
 class Cell:
-    side = 20
-    width = 2
-    is_filled = False
+    # Set parameters of the cell
+    side = 20  # size of the cell side
+    width = 2  # width of the cell's border
+    gap = 2  # width of the gap between cell's border and filled cell
+    fill_side = side - 2 * (width + gap)  # size of the filled cell size
+
+    is_filled = False  # checks if the cell is filled
 
     def __init__(self, x, y):
         self.coordinates = x, y
+        self.fill_coordinates = x + self.width + self.gap, y + self.width + self.gap
 
     def draw_cell(self):
         pygame.draw.rect(WIN, WHITE, (*self.coordinates, self.side, self.side), self.width)
 
     def fill_cell(self):
-        pygame.draw.rect(WIN, WHITE,
-                         (self.coordinates[0] + (self.width + 2), self.coordinates[1] + (self.width + 2), self.side
-                          - (2 * (self.width + 2)), self.side - (2 * (self.width + 2))))
+        pygame.draw.rect(WIN, WHITE, (*self.fill_coordinates, self.fill_side, self.fill_side))
         self.is_filled = True
 
     def empty_cell(self):
-        pygame.draw.rect(WIN, BLACK,
-                         (self.coordinates[0] + (self.width + 2), self.coordinates[1] + (self.width + 2), self.side
-                          - (2 * (self.width + 2)), self.side - (2 * (self.width + 2))))
+        pygame.draw.rect(WIN, BLACK, (*self.fill_coordinates, self.fill_side, self.fill_side))
         self.is_filled = False
 
     def hover_cell(self):
-        pygame.draw.rect(WIN, BLUE,
-                         (self.coordinates[0] + (self.width + 2), self.coordinates[1] + (self.width + 2), self.side
-                          - (2 * (self.width + 2)), self.side - (2 * (self.width + 2))))
+        pygame.draw.rect(WIN, BLUE, (*self.fill_coordinates, self.fill_side, self.fill_side))
 
     def clickable_area(self):
-        clickable_area = *self.coordinates, self.coordinates[0] + self.side, self.coordinates[1] + self.side
+        clickable_area = (*self.fill_coordinates, self.fill_coordinates[0] + self.fill_side, self.fill_coordinates[1]
+                          + self.fill_side)
         return clickable_area
 
     def hover_area(self):
-        hover_area = *self.coordinates, self.side, self.side
+        hover_area = *self.fill_coordinates, self.fill_side, self.fill_side
         return hover_area
+
+
+class Grid:
+    def __init__(self, cols, rows, cell_side):
+        self.cols = cols
+        self.rows = rows
+        self.grid = [[Cell(CENTER[0] + i * cell_side, CENTER[1] + j * cell_side) for j in range(cols)] for i in
+                     range(rows)]
+
+    def draw_grid(self):
+        for row in self.grid:
+            for cell in row:
+                cell.draw_cell()
+
+    def grid_hover(self):
+        for row in self.grid:
+            for cell in row:
+                cell_size = pygame.Rect(cell.hover_area())
+                if not cell.is_filled:
+                    if cell_size.collidepoint(pygame.mouse.get_pos()):
+                        cell.hover_cell()
+                    else:
+                        cell.empty_cell()
 
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
-    box = Cell(*CENTER)
-    box.draw_cell()
-    cell_size = pygame.Rect(*box.hover_area())
+    grid = Grid(20, 20, 18)
 
     while run:
         clock.tick(60)
@@ -67,24 +89,24 @@ def main():
                 run = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
 
-                # Get the position of the click
-                x, y = event.pos
+                for row in grid.grid:
+                    for cell in row:
 
-                # Check if the click is within the clickable area
-                if (box.clickable_area()[0] <= x <= box.clickable_area()[2] and box.clickable_area()[1]
-                        <= y <= box.clickable_area()[3]):
-                    if event.button == 1:  # Left click
-                        print("Left click detected at position", event.pos)
-                        box.fill_cell()
-                    if event.button == 3:  # Right click
-                        print("Right click detected at position", event.pos)
-                        box.empty_cell()
+                        # Get the position of the click
+                        x, y = event.pos
 
-        if not box.is_filled:
-            if cell_size.collidepoint(pygame.mouse.get_pos()):
-                box.hover_cell()
-            else:
-                box.empty_cell()
+                        # Check if the click is within the clickable area
+                        if (cell.clickable_area()[0] <= x <= cell.clickable_area()[2] and cell.clickable_area()[1]
+                                <= y <= cell.clickable_area()[3]):
+                            if event.button == 1:  # Left click
+                                print("Left click detected at position", event.pos)
+                                cell.fill_cell()
+                            if event.button == 3:  # Right click
+                                print("Right click detected at position", event.pos)
+                                cell.empty_cell()
+
+        grid.draw_grid()
+        grid.grid_hover()
 
         pygame.display.update()
 
